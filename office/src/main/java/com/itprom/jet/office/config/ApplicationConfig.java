e26b85bafea8b3352a560baea1afb6fa67efbdf7
Nikita Novikov <monstars51@gmail.com>
2024-01-30 17:31:16 +0300

    add frontend

--- office/src/main/java/com/itprom/jet/office/config/ApplicationConfig.java
+++ office/src/main/java/com/itprom/jet/office/config/ApplicationConfig.java
@@ -5,20 +5,20 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import com.itprom.jet.common.processor.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketSession;
 
-import java.net.http.WebSocket;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean
-    public MessageConverter converter(){
+    public MessageConverter converter() {
         return new MessageConverter();
     }
 
     @Bean
-    public Cache<String, WebSocket> sessionCache(){
+    public Cache<String, WebSocketSession> sessionCache() {
         return Caffeine.newBuilder()
                 .expireAfterWrite(20, TimeUnit.MINUTES)
                 .build();
--- office/src/main/java/com/itprom/jet/office/config/OfficeSocketConfig.java
+++ office/src/main/java/com/itprom/jet/office/config/OfficeSocketConfig.java
@@ -1,8 +1,5 @@
 package com.itprom.jet.office.config;
 
-import com.itprom.jet.common.processor.MessageConverter;
-import lombok.RequiredArgsConstructor;
-import org.apache.kafka.common.cache.Cache;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.socket.WebSocketSession;
@@ -10,17 +7,24 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.itprom.jet.common.processor.MessageConverter;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSocket
 @RequiredArgsConstructor
 public class OfficeSocketConfig implements WebSocketConfigurer {
+
     private final MessageConverter messageConverter;
     private final Cache<String, WebSocketSession> sessionCache;
     private final KafkaTemplate<String, String> kafkaTemplate;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry
-                .addHandler(new OfficeSocketHandler(messageConverter, sessionCache,kafkaTemplate), "/websocket")
+                .addHandler(new OfficeSocketHandler(messageConverter, sessionCache, kafkaTemplate),"/websocket")
                 .setAllowedOrigins("*");
     }
 }
